9df4b824dc24f93e3951e77c74d4dbecb3f4f4fb
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-01-27 17:42:15 -0600

    Prepping for builds with CI

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 var db = require('.');
 
-(async function () {
-    let connection = db.open({
-        Parameters: "apiKey=AIzaSyD1ypTmnJb_d8ZOyfc-KBMe0tw8owYCwjA",
-        Hostname: 'statesdemo',
-        Database: 'ewJviY6wboTKJ57A2dZkvq8kxYo1',
-        Username: 'user@example.com',
-        Password: 'password'
-    });
-    let stmt, results;
-    try {
-        results = await connection.query('SELECT * FROM states ORDER BY Ranking DESC');
-        console.log(results);
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await connection.close();
-        setTimeout(() => { process.exit(); }, 0)
+var connection = db.open({
+    Parameters: "apiKey=AIzaSyD1ypTmnJb_d8ZOyfc-KBMe0tw8owYCwjA",
+    Hostname: 'statesdemo',
+    Database: 'ewJviY6wboTKJ57A2dZkvq8kxYo1',
+    Username: 'user@example.com',
+    Password: 'password'
+});
+
+function handleError(error) {
+    console.log("ERROR:", error);
+    process.exit(1);
+}
+
+connection.query("SELECT * FROM states WHERE State = 'South Dakota'").then((data) => {
+    if (data.length != 1) {
+        handleError(new Error("Invalid data returned"));
     }
-})();
+    connection.close().then(() => {
+        process.exit(0);
+    }).catch(handleError);
+}).catch(handleError);
